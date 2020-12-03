7b823a72372767690616909f4eb6d0b39ea312b9
darien bryant <darienbryant11@gmail.com>
2020-12-03 15:55:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,12 @@
 def nyc_pigeon_organizer(data)
 
-final_results = data.each_with_object ({}) do |(key, value), final_array|
+  final_results = data.each_with_object ({}) do |(key, value), final_array|
 
-final_array value.each do |inner_key, value|
-  
-  
-end 
-
-
-end 
-
-
+      value.each do |inner_key, names|
+        names.each do |name|
+   
+   
+        end 
+    end 
+  end 
 end
